be4b5c2d06f78512dbc7e23af373ec56627bf8e1
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-04-13 22:55:45 +0500

    Функция std::shuffle
    Функция std::shuffle из стандартной библиотеки C++ используется для перемешивания элементов в диапазоне в случайном порядке.

---
+++ cpp/std_shuffle.py
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main() {
+        std::vector<int> v = {1, 2, 3, 4, 5};
+
+        std::shuffle(v.begin(), v.end());
+
+        for (int i : v) {
+            std::cout << i << " ";
+        }
+
+        std::cout << std::endl;
+
+        return ;
+}
