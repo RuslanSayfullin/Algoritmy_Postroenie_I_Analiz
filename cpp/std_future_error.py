76918aad190421415ed9cfb22ee952beefa6d1f9
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-04-17 23:28:36 +0500

    Класс std::future_error представляет собой исключение, которое выбрасывается в случае ошибки при использовании функций библиотеки потоков, связанных с асинхронным выполнением и общими состояниями (std::future, std::promise и т. д.). Подобно std::system_error, это исключение содержит код ошибки, совместимый с std::error_code.

---
+++ cpp/std_future_error.py
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <future>
+
+int main() {
+        std::future<int> future = std::async(std::launch::async, [] () {
+            return 1 / 0;
+        });
+
+        try {
+            int value = future.get();
+        } catch (std::future_error& e) {
+            std::cout << e.what() << std::endl;
+        }
+
+        return 0;
+}
