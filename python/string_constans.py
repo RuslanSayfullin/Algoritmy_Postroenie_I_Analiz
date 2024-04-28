31ed2f71b601548200b5bd0f066308aa23132dd0
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-04-28 23:48:07 +0500

    Константы пакета string
    Встроенный модуль string предоставляет расширенный функционал для работы со строками. Про форматируемые шаблоны из этого пакета мы уже рассказывали ранее.
    
    Сейчас поговорим про константы этого модуля. Из интересного стоит выделить ascii_letters, punctuation, digits и whitespace — символы ascii, знаки пунктуации, цифры и пробельные символы.
    
    Применение подобных констант может быть не очевидно с первого взгляда. Но подобные значения часто используются, например, в работе с алфавитом или в условном генераторе паролей.
    
    К тому же часто бывает, что разработчики делают костыли и создают свои собственные константы в коде вместо того, чтобы импортировать уже готовые из модуля string.

---
+++ python/string_constans.py
@@ -0,0 +1,15 @@
+import string
+
+# буквенные символы ascii 
+print(string.ascii_le00tters)
+
+# символы пунктуации
+print(string.punctuation)
+
+# символы цифр
+print(string.digits)
+
+# символы пробела
+print(string.whitespace)
+
+
