ddf2a2be04e83f90c8054d223ccf88fc5feab6ad
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-08-03 22:33:59 +0500

    Функция std::nexttoward() возвращает следующее представимое значение после x в направлении y. Эта функция ведет себя аналогично функции std::nextafter(), но с потенциально более точным y.

---
+++ std_nexttoward.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+	double x = 0.0;
+
+	std::cout << "Первое представимое значение больше нуля: " << std::nexttoward(x, 1.0L) << std::endl;
+	std::cout << "Первое представимое значение меньше нуля: " << std::nexttoward(x, -1.0L) <<std::endl;
+	return 0;
+}
